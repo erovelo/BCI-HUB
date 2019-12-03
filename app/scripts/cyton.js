0a8d82f9f5d6dbf2cfd9cec041b63fcb90dadc8d
Eric Rovelo <eric.1160@hotmail.com>
2019-12-03 01:56:21 -0600

    modo consola

--- app/scripts/cyton.js
+++ app/scripts/cyton.js
@@ -10,13 +10,14 @@ let streamCallback = undefined;
 function CytonGetPorts(callback) {
     ourBoard.listPorts().then(allPorts => {
         if(allPorts){
-            console.log(allPorts);
+            //console.log(allPorts);
             // Obtenemos puertos que tengan un puerto valido
             const ports = allPorts.filter(p => p.serialNumber != undefined);
             if(callback != undefined) callback(ports);
         }
     })
 }
+exports.CytonGetPorts = CytonGetPorts;
 
 /**
  * @returns {boolean} Si está conectado a un dispositivo o emulador, devuelve true
@@ -24,20 +25,24 @@ function CytonGetPorts(callback) {
 function CytonIsConnected() {
     return ourBoard.isConnected();
 }
+exports.CytonIsConnected = CytonIsConnected;
 
 // Devuelve si está transmitiendo Cyton
 function CytonIsStreaming() {
     return ourBoard.isStreaming();
 }
+exports.CytonIsStreaming = CytonIsStreaming;
 
 // Devuelve si está simulando a la tarjeta Cyton
 function CytonIsSimulating(){
     return ourBoard.isSimulating();
 }
+exports.CytonIsSimulating = CytonIsSimulating;
 
 function CytonGetNumberOfChannels() {
     return ourBoard.numberOfChannels();
 }
+exports.CytonGetNumberOfChannels = CytonGetNumberOfChannels;
 
 // Se conecta a la tarjeta Cyton
 function CytonConnect(portName, onSample, callback){
@@ -49,6 +54,7 @@ function CytonConnect(portName, onSample, callback){
         if(callback != undefined) callback(ourBoard.isConnected());
     });
 }
+exports.CytonConnect = CytonConnect; 
 
 function CytonStartStreaming(callback) {
     ourBoard.streamStart();
@@ -56,14 +62,17 @@ function CytonStartStreaming(callback) {
     
     if(callback != undefined) callback(ourBoard.isStreaming());
 }
+exports.CytonStartStreaming = CytonStartStreaming;
 
 function CytonStopStreaming(callback) {
     ourBoard.streamStop();
     if(callback != undefined) callback(!ourBoard.isStreaming());
 }
+exports.CytonStopStreaming = CytonStopStreaming;
 
 function CytonDisconnect(callback) {
     ourBoard.disconnect().then(() => {
         if(callback != undefined) callback(!ourBoard.isConnected());        
     })
 }
+exports.CytonDisconnect = CytonDisconnect;
